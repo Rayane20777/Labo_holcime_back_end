fe57b7a683db4e0a0ed0e260c0df91a5f4c2d966
Rayane <you@example.com>
2024-05-15 13:27:08 +0100

    database created only lpee table attributes left

---
+++ database/migrations/2024_05_09_131648_create_phase_gachages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phase_gachages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('temperature')->nullable();
+            $table->float('temperature_salle')->nullable();
+            $table->float('humidite')->nullable();
+            $table->float('p_prisme')->nullable();
+            $table->time('temps_gachage')->nullable();
+            $table->time('temps_casse')->nullable();
+            $table->foreignId('analyse_id')->constrained('analyses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phase_gachages');
+    }
+};
---
+++ database/migrations/2024_05_09_131932_create_resultat_analyse_physiques_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resultat_analyse_physiques', function (Blueprint $table) {
+            $table->id();
+            $table->float('1j')->nullable();
+            $table->float('2j')->nullable();
+            $table->float('7j')->nullable();
+            $table->float('28j')->nullable();
+            $table->float('90j')->nullable();
+            $table->foreignId('analyse_id')->constrained('analyses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resultat_analyse_physiques');
+    }
+};
