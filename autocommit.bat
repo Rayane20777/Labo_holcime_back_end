59ac419f02bff96ce82f06056e5d92c827542818
Rayane <you@example.com>
2024-05-16 21:13:01 +0100

    register working on back end now fixing the login issues

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "working on authentification"     
+git commit -m "register working on back end now fixing the login issues"     
 git push origin main                    
