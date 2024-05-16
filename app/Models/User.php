aaf89deeb3b34ffdcf468746c0ed7c62a3d9b105
Rayane <you@example.com>
2024-05-16 09:18:00 +0100

    your commit message

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
