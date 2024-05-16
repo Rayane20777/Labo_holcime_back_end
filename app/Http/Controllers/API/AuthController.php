2fc562e4d7f1e5579047f8a49596e799b66032ab
Rayane <you@example.com>
2024-05-16 10:58:02 +0100

    working on authentification

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
+use App\Models\Role;
+use App\Models\User;
 class AuthController extends Controller
 {
     
