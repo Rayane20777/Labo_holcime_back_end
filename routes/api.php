64a72e7ab4258eaa6f62dbabcf0bf1d0bf1ab3bd
Rayane <you@example.com>
2024-05-16 11:03:02 +0100

    working on authentification

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group([
+    'middleware' => ['api'],
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [App\Http\Controllers\API\AuthController::class, 'login']);
+Route::post('register', [App\Http\Controllers\API\AuthController::class, 'register']);
 });
