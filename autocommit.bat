5afdb282b1ec8aa5a64a5092ddd259d257bd17d4
Rayane <you@example.com>
2024-05-16 10:33:02 +0100

    working on authentification

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "quelque moditfication"     
+git commit -m "working on authentification"     
 git push origin main                    
