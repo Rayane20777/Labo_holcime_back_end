e25c1b1887b935a6a5d49369a61f219ae9c0a4a3
Rayane <you@example.com>
2024-05-17 11:53:01 +0100

    fixed login and now making factories

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "debigging the loging"     
+git commit -m "fixed login and now making factories"     
 git push origin main                    
