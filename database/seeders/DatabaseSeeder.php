dffb1df33dd892bc8bb75b665fddecffa162b4fe
Rayane <you@example.com>
2024-05-17 12:23:01 +0100

    setting up seeders and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,12 @@ class DatabaseSeeder extends Seeder
         Role::factory()->create([
             'name' => 'user',
         ]);
+        Role::factory()->create([
+            'name' => 'admin',
+        ]);
+        Role::factory()->create([
+            'name' => 'super_admin',
+        ]);
 
         User::factory(10)->create();
 
