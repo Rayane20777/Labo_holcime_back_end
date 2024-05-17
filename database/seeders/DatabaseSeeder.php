af6e2f61c9f42a97d3d4506510184542531e9c55
Rayane <mustaphagameover111@example.com>
2024-05-17 12:18:01 +0100

    setting up seeders and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,22 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Role;
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
     public function run(): void
-    {
+    {   
+        Role::factory()->create([
+            'name' => 'user',
+        ]);
+
+        User::factory(10)->create();
+
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
