d750cfcbaf8e837d4375830e5931eeb4124d08e8
Rayane <mustaphagameover111@example.com>
2024-05-20 15:20:12 +0100

    pourcentage calculating done for each attribute seperated

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "fixed a missing request"     
+git commit -m "pourcentage calculating done for each attribute seperated"     
 git push origin main                    
