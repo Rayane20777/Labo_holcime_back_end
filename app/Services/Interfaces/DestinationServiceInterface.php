43e68f3b70d8802e8f1220e73cfe3f19422ffa8e
Rayane <mustaphagameover111@example.com>
2024-05-20 09:18:47 +0100

    Destination CRUD done

--- app/Services/Interfaces/DestinationServiceInterface.php
+++ app/Services/Interfaces/DestinationServiceInterface.php
@@ -6,9 +6,9 @@ use App\DTOs\DestinationDTO;
 
 interface DestinationServiceInterface
 {
+    public function all();
     public function store(DestinationDTO $data);
     public function edit(DestinationDTO $data);
     public function destroy(int $id);
     public function restore(int $id);
-    public function all();
 }
--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "Destination CRUD in progress"     
+git commit -m "Destination CRUD done"     
 git push origin main                    
