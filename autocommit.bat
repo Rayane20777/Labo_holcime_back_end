7e83a5748b9d1451a7672083b60fc74339449009
Rayane <you@example.com>
2024-05-21 10:48:40 +0100

    all edit updated and working

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "debuging the proportion edit"     
+git commit -m "all edit updated and working "     
 git push origin main                    
