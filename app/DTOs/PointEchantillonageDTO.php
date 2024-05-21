c42a11e57043a94b74f9f476797441ccc363e36f
Rayane <mustaphagameover111@example.com>
2024-05-21 09:28:03 +0100

    fixing all edits to exclude the dto

--- app/DTOs/PointEchantillonageDTO.php
+++ app/DTOs/PointEchantillonageDTO.php
@@ -16,12 +16,12 @@ class PointEchantillonageDTO {
         );
     }
 
-    public static function fromEdit(array $data): self
-    {
-        return new self(
-            id: $data['id'],
-            nom: $data['nom'],
-            matiere_id: $data['matiere_id'] ?? null
-        );
-    }
+    // public static function fromEdit(array $data): self
+    // {
+    //     return new self(
+    //         id: $data['id'],
+    //         nom: $data['nom'],
+    //         matiere_id: $data['matiere_id'] ?? null
+    //     );
+    // }
 }
