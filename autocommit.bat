8003ad6efe4a21465087caedc7cc877610066fc8
Rayane <mustaphagameover111@example.com>
2024-05-21 12:44:18 +0100

    phase prise temps crud done

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "phase gachage crud done"     
+git commit -m "phase prise temps crud done"     
 git push origin main                    
