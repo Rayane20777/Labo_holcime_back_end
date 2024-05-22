41b0aa1ac69153d170869241f7b718d9cd2ce6f3
Rayane <mustaphagameover111@example.com>
2024-05-22 09:48:04 +0100

    getting all the data going from the matiere

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::controller(AuthController::class)->group(function () {
 
 
 Route::get('/matiere', [MatiereController::class, 'index']);
+Route::get('/matiere/{$id}', [MatiereController::class, 'matiereFilter']);
 Route::post('/matiere', [MatiereController::class, 'store']);
 Route::post('/matiere/{id}', [MatiereController::class, 'edit']);
 Route::delete('/matiere/{id}', [MatiereController::class, 'destroy']);
