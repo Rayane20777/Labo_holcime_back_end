63f007b3b6b079e4aa4b8b928667f85ec9868bfd
Rayane <mustaphagameover111@example.com>
2024-05-23 10:13:02 +0100

    working on XRF Analayse

--- app/Http/Controllers/XrfController.php
+++ app/Http/Controllers/XrfController.php
@@ -2,10 +2,78 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Traits\ResponseTrait;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\XrfRequest;
+use App\Services\Interfaces\XrfServiceInterface;
+use App\DTOs\XrfDTO;
+use Exception;
 
 class XrfController extends Controller
 {
-    //
+    use ResponseTrait;
+
+    private XrfServiceInterface $service;
+
+    public function __construct(XrfServiceInterface $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(): JsonResponse
+    {
+        try {
+            $data = $this->service->all();
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess($data, "Xrf retrieved successfully");
+    }
+
+    public function store(XrfRequest $request): JsonResponse
+    {
+        $payload = XrfDTO::fromAdd($request->all());
+
+        try {
+            $data = $this->service->store($payload);
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess($data, "Xrf created successfully");
+    }
+
+    public function edit(XrfRequest $request, int $id)
+    {
+        try{
+           $data = $this->service->edit($request->all(),$id);
+        }catch(Exception $e){
+            return $this->responseError($e->getMessage());
+        }
+        return $this->responseSuccess($data, "Xrf updated successfully");
+
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        try {
+            $this->service->destroy($id);
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess(null, "Xrf deleted successfully");
+    }
+
+    public function restore(int $id): JsonResponse
+    {
+        try {
+            $this->service->restore($id);
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess(null, "Xrf restored successfully");
+    } 
 }
--- app/Models/Xrf.php
+++ app/Models/Xrf.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\OneToOneTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Xrf extends Model
 {
-    use HasFactory;
+    use HasFactory,OneToOneTrait,SoftDeletes;
+
+    protected $table = 'analyse_chimiques';
+    protected $fillable = [
+        
+        'SiO2',
+        'Al2O3',
+        'Fe2O3',
+        'CaO',
+        'SO3',
+        'MgO',
+        'K2O',
+        'Na2O',
+        'P2O5',
+        'analyse_id'
+    ];
 }
