0c2d744aed5bb20de08e7eaa2d3018d2fdc4f2db
Rayane <mustaphagameover111@example.com>
2024-05-23 10:08:02 +0100

    working on XRF Analayse

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "analyse status change done"     
+git commit -m "working on XRF Analayse "     
 git push origin main                    
