dd4e6eca077b6b1ef1a0304714e9c7a8e898d4de
Rayane <mustaphagameover111@example.com>
2024-05-23 15:23:03 +0100

    makin the xrd analyse

---
+++ app/Http/Controllers/XrdController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ResponseTrait;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\XrdRequest;
+use App\Services\Interdaces\XrdServiceInterdace;
+use App\DTOs\XrdDTO;
+use Exception;
+
+class XrdController extends Controller
+{
+    use ResponseTrait;
+
+    private XrdServiceInterdace $service;
+
+    public function __construct(XrdServiceInterdace $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(): JsonResponse
+    {
+        try {
+            $data = $this->service->all();
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess($data, "Xrd retrieved successfully");
+    }
+
+    public function store(XrdRequest $request): JsonResponse
+    {
+        $payload = XrdDTO::fromAdd($request->all());
+
+        try {
+            $data = $this->service->store($payload);
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess($data, "Xrd created successfully");
+    }
+
+    public function edit(XrdRequest $request, int $id)
+    {
+        try{
+           $data = $this->service->edit($request->all(),$id);
+        }catch(Exception $e){
+            return $this->responseError($e->getMessage());
+        }
+        return $this->responseSuccess($data, "Xrd updated successfully");
+
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        try {
+            $this->service->destroy($id);
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess(null, "Xrd deleted successfully");
+    }
+
+    public function restore(int $id): JsonResponse
+    {
+        try {
+            $this->service->restore($id);
+        } catch (Exception $e) {
+            return $this->responseError($e->getMessage());
+        }
+
+        return $this->responseSuccess(null, "Xrd restored successfully");
+    } 
+}
