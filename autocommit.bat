5da666a425cdd4c09fdfc7ae4b4d489a5bc5f157
Rayane <you@example.com>
2024-05-23 15:13:03 +0100

    makin the xrd analyse

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "modifiying the analyse chimique "     
+git commit -m "makin the xrd analyse "     
 git push origin main                    
