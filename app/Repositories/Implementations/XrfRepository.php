ad9ce3b0b3074c379304d3bd157e84a17f07a11c
Rayane <you@example.com>
2024-05-23 10:33:02 +0100

    working on XRF Analayse

--- app/Repositories/Implementations/XrfRepository.php
+++ app/Repositories/Implementations/XrfRepository.php
@@ -32,8 +32,8 @@ class XrfRepository implements XrfRepositoryInterface
 
     public function edit($data, $id)
     {
-        $analyse_chimique = Xrf::where('id',$id)->first();
-         $analyse_chimique->update([
+        $xrf = Xrf::where('id',$id)->first();
+         $xrf->update([
             'SiO2' => $data->SiO2 ?? 0,
             'Al2O3' => $data->Al2O3 ?? 0,
             'Fe2O3' => $data->Fe2O3 ?? 0,
@@ -45,19 +45,19 @@ class XrfRepository implements XrfRepositoryInterface
             'P2O5' => $data->P2O5 ?? 0, 
             'analyse_id' => $data['analyse_id'],
         ]);
-        return $analyse_chimique;
+        return $xrf;
 
     }
 
     public function destroy(int $id)
     {
-        $analyse_chimique = Xrf::findOrFail($id);
-        return $analyse_chimique->delete();
+        $xrf = Xrf::findOrFail($id);
+        return $xrf->delete();
     }
 
     public function restore(int $id)
     {
-        $analyse_chimique = Xrf::withTrashed()->findOrFail($id);
-        return $analyse_chimique->restore();
+        $xrf = Xrf::withTrashed()->findOrFail($id);
+        return $xrf->restore();
     }
 }
