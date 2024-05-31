58eced731a3f14320ebb44ed3d84d0c3b727c63c
Rayane <mustaphagameover111@example.com>
2024-05-31 10:38:03 +0100

    edit

--- autocommit.bat
+++ autocommit.bat
@@ -2,5 +2,5 @@ cd ..
 cd ..
 cd Users\musta\OneDrive\Desktop\Labo_Lafarge_back_end
 git add .                             
-git commit -m "finishing then factories and adding small things"     
+git commit -m "edit"     
 git push origin main                    
